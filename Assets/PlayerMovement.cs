d213f8c4680c4b1d67ef9185be6309efd2d4c8d2
Joshua Reimbeau <jreimbe1@students.kennesaw.edu>
2026-02-15 10:46:17 -0500

    Added a tilemap and started making basic movement to go through the test stage

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform tf;
+    private Rigidbody2D rb;
+    private float movex, movey;
+    public float speed = 5;
+    private float health = 100;
+    bool isGrounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(movex, movey, 0);
+        //move = tf.TransformDirection(move);
+        tf.position += move * speed * Time.fixedDeltaTime;
+    }
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movex = movementVector.x;
+        //movey = movementVector.y;
+    }
+    void OnJump()
+    {
+            rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+            isGrounded = false;
+    }
+    public void Heal(float amount)
+    {
+        health += amount;
+        if (health > 100)
+        {
+            health = 100;
+        }
+        //healthText.text = "Health: " + health;
+    }
+}
