54fafad3db8584b0f7002db7ea188c916a863a34
Joshua Reimbeau <jreimbe1@students.kennesaw.edu>
2026-02-16 13:08:32 -0500

    Trying and failing to make slides work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,28 +2,75 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using static Unity.Burst.Intrinsics.X86.Avx;
+using static UnityEngine.UI.Image;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Transform tf;
     private Rigidbody2D rb;
     private float movex, movey;
-    public float speed = 5;
+    float speed = 5;
+    string weight = "med"; // "low", "med", "high"
+    float lowWeightSpeed = 12.5f;
+    float medWeightSpeed = 10;
+    float highWeightSpeed = 5f;
     private float health = 100;
 
     public ContactFilter2D groundFilter; // Set in Inspector (layer & angle)
     bool IsGrounded => rb.IsTouching(groundFilter);
+
+    private RaycastHit2D hit; // Store the result of the raycast
+    private float slopeMultiplier = 10000; // Adjust this value to control how much the slope affects speed
+    bool isOnSlope = false; // Track if the player is currently on a slope
+    public TMPro.TextMeshProUGUI onSlopeText;
+    public LayerMask groundLayer; // Set this to the layer(s) that represent the ground in your game
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         tf = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        onSlopeText = GameObject.Find("Slope (TMP)").GetComponent<TMPro.TextMeshProUGUI>();
     }
     void FixedUpdate()
     {
+        onSlopeText.text = "On Slope: " + isOnSlope;
+        switch (weight)
+        {
+            case "low":
+                speed = lowWeightSpeed;
+                break;
+            case "med":
+                speed = medWeightSpeed;
+                break;
+            case "high":
+                speed = highWeightSpeed;
+                break;
+            default:
+                speed = medWeightSpeed;
+                break;
+        }
+
         Vector3 move = new Vector3(movex, movey, 0);
         tf.position += move * speed * Time.fixedDeltaTime;
+
+        if (IsGrounded)
+        {
+            float rayDistance = 2f;
+            Vector2 origin = transform.position;
+
+            Debug.DrawRay(origin, Vector2.down * rayDistance, Color.green);
+
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, rayDistance, groundLayer);
+
+            if (hit.collider != null)
+            {
+                float angle = Vector2.Angle(hit.normal, Vector2.up);
+                Debug.Log("Hit: " + hit.collider.name + " angle: " + angle);
+                isOnSlope = angle > 0; // Consider it a slope if the angle is greater than 0
+            }
+        }
     }
     private void OnMove(InputValue movementValue)
     {
@@ -36,6 +83,30 @@ public class PlayerMovement : MonoBehaviour
         if (IsGrounded)
         {
             rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnSlide()
+    {   
+        if (isOnSlope)
+        {
+            Vector2 normal2D = hit.normal.normalized;
+            Vector2 tangent2D = new Vector2(-normal2D.y, normal2D.x); // perpendicular to normal
+
+            // Your intended move direction in 2D
+            Vector2 moveDir2D = new Vector2(movex, movey).normalized;
+
+            // How much you're moving along the slope (signed)
+            float slopeFactor = Vector2.Dot(moveDir2D, tangent2D);
+
+            // Apply slope-based speed boost (use a baseSpeed so it doesn't explode)
+            float finalSpeed = speed * (1f + slopeFactor * slopeMultiplier);
+
+            // Move along your original move vector
+            Vector3 move = new Vector3(movex, movey, 0f).normalized;
+            tf.position += move * finalSpeed * Time.fixedDeltaTime;
+            rb.AddForce(tangent2D * slopeFactor * slopeMultiplier, ForceMode2D.Force);
+            Debug.Log("Moving on slope with final speed: " + finalSpeed);
         }
     }
     public void Heal(float amount)
