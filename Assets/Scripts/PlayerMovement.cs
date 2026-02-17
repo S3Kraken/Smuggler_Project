aa3d1d370f05203ed2f486dea0dd9193c8a0755d
Joshua Reimbeau <jreimbe1@students.kennesaw.edu>
2026-02-16 19:23:53 -0500

    Pushing so I can try a character controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,34 +23,47 @@ public class PlayerMovement : MonoBehaviour
     private RaycastHit2D hit; // Store the result of the raycast
     private float slopeMultiplier = 10000; // Adjust this value to control how much the slope affects speed
     bool isOnSlope = false; // Track if the player is currently on a slope
-    public TMPro.TextMeshProUGUI onSlopeText;
+    //public TMPro.TextMeshProUGUI onSlopeText;
     public LayerMask groundLayer; // Set this to the layer(s) that represent the ground in your game
+
+    public bool canClimb = false;
+    public TMPro.TextMeshProUGUI ChangeWeightButtonText;
+
+    bool slopeBoost = false;
+    float slopeBoostTimer = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         tf = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
-        onSlopeText = GameObject.Find("Slope (TMP)").GetComponent<TMPro.TextMeshProUGUI>();
+        //onSlopeText = GameObject.Find("Slope (TMP)").GetComponent<TMPro.TextMeshProUGUI>();
+        ChangeWeightButtonText = GameObject.Find("ChangeWeightButton (TMP)").GetComponent<TMPro.TextMeshProUGUI>();
+        ChangeWeightButtonText.text = weight;
     }
     void FixedUpdate()
     {
-        onSlopeText.text = "On Slope: " + isOnSlope;
-        switch (weight)
+        //onSlopeText.text = "On Slope: " + isOnSlope;
+        slopeBoostTimer += Time.deltaTime;
+        if (slopeBoostTimer > 1f)
         {
-            case "low":
-                speed = lowWeightSpeed;
-                break;
-            case "med":
-                speed = medWeightSpeed;
-                break;
-            case "high":
-                speed = highWeightSpeed;
-                break;
-            default:
-                speed = medWeightSpeed;
-                break;
+            switch (weight)
+            {
+                case "low":
+                    speed = lowWeightSpeed;
+                    break;
+                case "med":
+                    speed = medWeightSpeed;
+                    break;
+                case "high":
+                    speed = highWeightSpeed;
+                    break;
+                default:
+                    speed = medWeightSpeed;
+                    break;
+            }
         }
+        
 
         Vector3 move = new Vector3(movex, movey, 0);
         tf.position += move * speed * Time.fixedDeltaTime;
@@ -76,7 +89,14 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movex = movementVector.x;
-        //movey = movementVector.y;
+        //if (canClimb)
+        //{
+        //    movey = 2;
+        //}
+        //else
+        //{
+        //    movey = 0;
+        //}
     }
     void OnJump()
     {
@@ -87,26 +107,29 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void OnSlide()
-    {   
+    {
         if (isOnSlope)
         {
-            Vector2 normal2D = hit.normal.normalized;
-            Vector2 tangent2D = new Vector2(-normal2D.y, normal2D.x); // perpendicular to normal
+            speed = 20f; // Increase speed when sliding down a slope
+            slopeBoost = true;
+            slopeBoostTimer = 0f;
+            //Vector2 normal2D = hit.normal.normalized;
+            //Vector2 tangent2D = new Vector2(-normal2D.y, normal2D.x); // perpendicular to normal
 
-            // Your intended move direction in 2D
-            Vector2 moveDir2D = new Vector2(movex, movey).normalized;
+            //// Your intended move direction in 2D
+            //Vector2 moveDir2D = new Vector2(movex, movey).normalized;
 
-            // How much you're moving along the slope (signed)
-            float slopeFactor = Vector2.Dot(moveDir2D, tangent2D);
+            //// How much you're moving along the slope (signed)
+            //float slopeFactor = Vector2.Dot(moveDir2D, tangent2D);
 
-            // Apply slope-based speed boost (use a baseSpeed so it doesn't explode)
-            float finalSpeed = speed * (1f + slopeFactor * slopeMultiplier);
+            //// Apply slope-based speed boost (use a baseSpeed so it doesn't explode)
+            //float finalSpeed = speed * (1f + slopeFactor * slopeMultiplier);
 
-            // Move along your original move vector
-            Vector3 move = new Vector3(movex, movey, 0f).normalized;
-            tf.position += move * finalSpeed * Time.fixedDeltaTime;
-            rb.AddForce(tangent2D * slopeFactor * slopeMultiplier, ForceMode2D.Force);
-            Debug.Log("Moving on slope with final speed: " + finalSpeed);
+            //// Move along your original move vector
+            //Vector3 move = new Vector3(movex, movey, 0f).normalized;
+            //tf.position += move * finalSpeed * Time.fixedDeltaTime;
+            //rb.AddForce(tangent2D * slopeFactor * slopeMultiplier, ForceMode2D.Force);
+            //Debug.Log("Moving on slope with final speed: " + finalSpeed);
         }
     }
     public void Heal(float amount)
@@ -117,5 +140,43 @@ public class PlayerMovement : MonoBehaviour
             health = 100;
         }
         //healthText.text = "Health: " + health;
+    }
+
+    public void ChangeWeight()
+    {
+        if (weight == "low")
+        {
+            weight = "med";
+        }
+        else if (weight == "med")
+        {
+            weight = "high";
+        }
+        else
+        {
+            weight = "low";
+        }
+        ChangeWeightButtonText.text = weight;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ladders")
+        {
+            canClimb = true;
+            //if w key is press go up
+            if (Input.GetKey(KeyCode.W)){
+                movey = 2;
+            }
+            else
+            {
+                movey = 0;
+            }
+        }
+        else
+        {
+            canClimb = false;
+            movey = 0;
+        }
     }
 }
