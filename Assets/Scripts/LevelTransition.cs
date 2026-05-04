2bbb6f275d9fb83471393b4ba838f3113763092b
S3Kraken <165493182+S3Kraken@users.noreply.github.com>
2026-05-04 10:23:25 -0400

    Working on getting stages working

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,25 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && collision.CompareTag("Player"))
+        {
+            switch (SceneManager.GetActiveScene().buildIndex)
+            {
+                case 2: //roof to alley
+                    SceneManager.LoadSceneAsync("AlleywayStage");
+                    break;
+                case 3: //alley to sewer
+                    SceneManager.LoadSceneAsync("SewerStage");
+                    break;
+                case 4: //sewer to roof
+                    SceneManager.LoadSceneAsync("RoofTopStage");
+                    break;
+            }
+        }
+    }
+}
